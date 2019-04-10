d83673346940f1acd59aec21132380b690c8b20b
Margaret oluwatotin Sippio <46166856+toymagucfdatascience@users.noreply.github.com>
2019-04-10 01:30:34 -0400

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,20 @@
+#%% Change working directory from the workspace root to the ipynb file location. Turn this addition off with the DataScience.changeDirOnImportExport setting
+import os
+try:
+	os.chdir(os.path.join(os.getcwd(), 'demo_assignment\Instructions\Images'))
+	print(os.getcwd())
+except:
+	pass
+
+#%%
+
+
+
+#%%
+
+
+
+#%%
+
+
+
